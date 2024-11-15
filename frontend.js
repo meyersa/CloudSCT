796121a53d75ef990350fb91d58f0e5c7ad13632
Meyers <augustmeyers14@gmail.com>
2024-11-14 21:57:50 -0500

    Cache results

--- frontend.js
+++ frontend.js
@@ -3,19 +3,26 @@ const path = require("path");
 const app = express();
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const NodeCache = require("node-cache");
 
 const serviceAccount = require("./auth.json");
+const cache = new NodeCache({ stdTTL: 3600 });
 
 initializeApp({
   credential: cert(serviceAccount),
 });
-
 const db = getFirestore();
 
 app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/get-data-files", async (req, res) => {
-  console.log("Returning data files")
+  console.log("Returning data files");
+
+  // Check cache first
+  const cachedFiles = cache.get("allDataFiles");
+  if (cachedFiles) {
+    return res.json(cachedFiles);
+  }
 
   try {
     const snapshot = await db.collection("data").get();
@@ -24,6 +31,10 @@ app.get("/get-data-files", async (req, res) => {
     }
 
     const files = snapshot.docs.map((doc) => doc.id);
+
+    // Store in cache
+    cache.set("allDataFiles", files);
+
     res.json(files);
   } catch (error) {
     console.error("Error fetching documents:", error);
@@ -32,9 +43,15 @@ app.get("/get-data-files", async (req, res) => {
 });
 
 app.get("/get-data-file", async (req, res) => {
-  console.log("Returning data file")
+  console.log("Returning data file");
 
   const { filename } = req.query;
+
+  // Check cache for the specific file
+  const cachedFile = cache.get(filename);
+  if (cachedFile) {
+    return res.json(cachedFile);
+  }
 
   try {
     const docRef = db.collection("data").doc(filename);
@@ -45,6 +62,9 @@ app.get("/get-data-file", async (req, res) => {
     }
 
     res.json(doc.data());
+
+    // Store in cache
+    cache.set(filename, data);
   } catch (error) {
     console.error("Error fetching document:", error);
     res.status(500).send("Error fetching data from Firestore");
